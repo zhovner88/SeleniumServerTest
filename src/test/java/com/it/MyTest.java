9afc378ac5659d5c3dff4aa28e9a9ed84f8d8a36
zhvoner <denys.zhovnerovych@gmail.com>
2019-03-13 13:03:31 +0200

    Basic project structure

--- src/test/java/com/it/MyTest.java
+++ src/test/java/com/it/MyTest.java
@@ -1,10 +1,18 @@
 package com.it;
 
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class MyTest {
     @Test
     public void testDoesNothing() {
-
+        System.setProperty("webdriver.chrome.driver",
+                "/home/denys/Documents/repositories/globalTraning/drivers/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://mvnrepository.com/");
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals("https://mvnrepository.com/", url);
+        driver.quit();
     }
 }
